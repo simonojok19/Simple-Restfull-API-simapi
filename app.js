92e2aa4ee0a9f6d226daca7f420549ad35dfc6db
Simon Peter Ojok <simonojok19@gmail.com>
2019-10-30 20:48:05 +0300

    initial

---
+++ app.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my api');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
